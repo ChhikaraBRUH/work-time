7d4c75e1e9109804d391d8ae29979d9c93b0eefa
Chaitanya <67656388+ChhikaraBRUH@users.noreply.github.com>
2022-07-19 17:34:54 +0530

    fix: link fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
 					<span className='text-2xl font-medium text-gray-600'>Pomodoro Timer</span>
 				</h1>
 			</Link>
-			<a href='https://github.com/ChhikaraBRUH/work-time' target='_blank'>
+			<a href='https://github.com/ChhikaraBRUH/work-time' target='_blank' rel='noreferrer'>
 				<button
 					type='button'
 					className='border border-indigo-600 bg-indigo-600 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-indigo-700 focus:outline-none focus:shadow-outline'>
